c9fbef8242298d4656e439b185134b2e76c88cf8
spriadka <spriadka@github.com>
2018-03-07 12:06:03 +0100

    added AddedTestCase

---
+++ src/test/java/com/spriadka/demos/AddedTestCase.java
@@ -0,0 +1,13 @@
+package com.spriadka.demos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddedTestCase {
+
+    @Test
+    public void should_pass_because_this_test_was_added() {
+        Assert.assertTrue(true);
+    }
+
+}
